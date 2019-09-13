c0fbcebfb158a6bb6f3ad700fbd6ec67fcc4f6d5
willeett <55180511+willeett@users.noreply.github.com>
2019-09-13 19:35:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(willeett, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),

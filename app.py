42183d900fbd61f1a7ba5a3140e42416b3e4e72e
willeett <55180511+willeett@users.noreply.github.com>
2019-09-13 21:45:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Import required libraries
+    # Import required libraries
 import os
 from random import randint
 
@@ -15,34 +15,18 @@ import dash_html_components as html
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(app.py)
+server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(app.py, server=server)
+app = dash.Dash(__name__, server=server)
+
 
 # Put your Dash code here
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
-
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+    
 
 # Run the Dash app
-if app.py == '__main__':
+if __name__ == '__main__':
     app.server.run(debug=True, threaded=True)

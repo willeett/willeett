6191e7a493139a71d8dc007b38c27ae814bae26a
willeett <55180511+willeett@users.noreply.github.com>
2019-09-14 10:03:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-    # Import required libraries
-import os
-from random import randint
-
-import plotly.plotly as py
-from plotly.graph_objs import *
-
-import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
+import flask
+import pandas as pd
+import time
+import os
 
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+server = flask.Flask('app')
+server.secret_key = os.environ.get('secret_key', 'secret')
 
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
 
-# Put your Dash code here
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
-    
+app = dash.Dash('app', server=server)
 
-# Run the Dash app
+app.scripts.config.serve_locally = False
+dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+
+app.layout = html.Div([
+    html.H1('Stock Tickers'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=[
+            {'label': 'Tesla', 'value': 'TSLA'},
+            {'label': 'Apple', 'value': 'AAPL'},
+            {'label': 'Coke', 'value': 'COKE'}
+        ],
+        value='TSLA'
+    ),
+    dcc.Graph(id='my-graph')
+], className="container")
+
+@app.callback(Output('my-graph', 'figure'),
+              [Input('my-dropdown', 'value')])
+def update_graph(selected_dropdown_value):
+    dff = df[df['Stock'] == selected_dropdown_value]
+    return {
+        'data': [{
+            'x': dff.Date,
+            'y': dff.Close,
+            'line': {
+                'width': 3,
+                'shape': 'spline'
+            }
+        }],
+        'layout': {
+            'margin': {
+                'l': 30,
+                'r': 20,
+                'b': 30,
+                't': 20
+            }
+        }
+    }
+
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    app.run_server()
